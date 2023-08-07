9c4a010ecf27080f25760a2d598128fd63ee8b54
TheiLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2023-08-07 12:00:02 +0000

    Add checks for when API server is not available

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ function wait_for_clusteroperators() {
   while true; do
     local clusteroperators_status
     clusteroperators_status=$(oc get clusteroperators)
+    if [[ $? != 0 ]]; then
+      echo "API Server not reachable"
+      sleep 2s
+    fi
     echo "$clusteroperators_status" | tail -n +2 | awk '{print $3}' | grep False &>/dev/null
     local ret1=$?
     echo "$clusteroperators_status" | tail -n +2 | awk '{print $4}' | grep True &>/dev/null
@@ -119,10 +123,18 @@ PASSWORD=$(crc console -ojson | jq -r '.clusterConfig.adminCredentials.password'
 
 echo ""
 
-echo "Console available here: https://console-openshift-console.apps.${BASE_DOMAIN}"
-echo "Console Login Credentials: "
-echo "USERNAME: ${USERNAME} PASSWORD: ${PASSWORD}"
+echo "The server is accessible via web console at:"
+echo "  https://console-openshift-console.apps.${BASE_DOMAIN}"
 
 echo ""
 
-echo "CLI Login Command: oc login -u ${USERNAME} -p ${PASSWORD} https://api.${BASE_DOMAIN}:6443"
+echo "Log in as administrator: "
+echo "  USERNAME: ${USERNAME}"
+echo "  PASSWORD: ${PASSWORD}"
+
+echo ""
+
+
+
+echo "Use the 'oc' command line interface:"
+echo "  $ oc login -u ${USERNAME} -p ${PASSWORD} https://api.${BASE_DOMAIN}:6443"
