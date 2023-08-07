73db88e41fc951635519a844e44c6e4a9b1748bc
TheiLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2023-08-07 11:29:55 +0000

    README + installation script

---
+++ README.md
@@ -0,0 +1,53 @@
+# remote-crc-setup
+
+Allows you to setup a remote crc cluster
+
+## Minimum requirements
+
+| Resource              | Required         |
+|-----------------------|------------------|
+| CPU                   | 4 Core / Threads |
+| RAM                   | 16GB             |
+| Disk                  | 80GB             |
+| OS                    | Rocky Linux 9    |
+| Nested Virtualization | Enabled          |
+
+## Pre-requisites
+
+### Create a new user and login
+
+- SSH as root
+
+```bash
+ssh root@<ip>
+```
+
+- Create the `crc` user
+
+```bash
+adduser crc
+echo password | passwd crc --stdin
+echo "crc ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/crc
+chmod 0440 /etc/sudoers.d/crc
+```
+
+## Installation
+
+- SSH as `crc` user created above (Do no use su -)
+
+- Run the installation script
+```bash
+curl https://raw.githubusercontent.com/iLLeniumStudios/remote-crc-setup/main/install.sh | bash
+```
+
+- Wait for the script to finish the installation. You should see something like this once everything is done:
+
+```bash
+Console available here: https://console-openshift-console.apps.10.0.6.188.nip.io
+Console Login Credentials:
+USERNAME: kubeadmin PASSWORD: vAFof-LsHb3-S2dv6-Yj3mF
+
+CLI Login Command: oc login -u kubeadmin -p vAFof-LsHb3-S2dv6-Yj3mF https://api.10.0.6.188.nip.io:6443
+```
+
+> Note that the script enables cluster monitoring components as well. If you don't need those, you can disable them from the `install.sh` script
---
+++ install.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+function wait_for_clusteroperators() {
+  while true; do
+    local clusteroperators_status
+    clusteroperators_status=$(oc get clusteroperators)
+    echo "$clusteroperators_status" | tail -n +2 | awk '{print $3}' | grep False &>/dev/null
+    local ret1=$?
+    echo "$clusteroperators_status" | tail -n +2 | awk '{print $4}' | grep True &>/dev/null
+    local ret2=$?
+    echo "$clusteroperators_status" | tail -n +2 | awk '{print $5}' | grep True &>/dev/null
+    local ret3=$?
+    if [[ $ret1 != 0 ]] && [[ $ret2 != 0 ]] && [[ $ret3 != 0 ]]; then
+      break
+    fi
+    echo "Waiting for the clusteroperators to be ready"
+    sleep 10s
+  done
+}
+
+# Install packages
+
+sudo dnf install jq wget -y
+
+# Disable firewalld
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo systemctl mask --now firewalld
+
+# Download crc
+CRC_VERSION=2.24.1
+wget https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/crc/${CRC_VERSION}/crc-linux-amd64.tar.xz
+tar -xvf crc-linux-amd64.tar.xz
+sudo mv crc-linux-${CRC_VERSION}-amd64/crc /usr/local/bin/
+sudo chmod +x /usr/local/bin/crc
+rm -rf crc-linux*
+
+# Configure crc
+crc config set memory 14336
+crc config set enable-cluster-monitoring true
+
+crc setup --log-level debug
+
+crc start --log-level debug
+
+# Setup haproxy
+
+sudo dnf install haproxy /usr/sbin/semanage -y
+sudo semanage port -a -t http_port_t -p tcp 6443
+sudo cp /etc/haproxy/haproxy.cfg{,.bak}
+
+CRC_IP=$(crc ip)
+sudo tee /etc/haproxy/haproxy.cfg &>/dev/null <<EOF
+global
+    log /dev/log local0
+
+defaults
+    balance roundrobin
+    log global
+    maxconn 100
+    mode tcp
+    timeout connect 5s
+    timeout client 500s
+    timeout server 500s
+
+listen apps
+    bind 0.0.0.0:80
+    server crcvm $CRC_IP:80 check
+
+listen apps_ssl
+    bind 0.0.0.0:443
+    server crcvm $CRC_IP:443 check
+
+listen api
+    bind 0.0.0.0:6443
+    server crcvm $CRC_IP:6443 check
+EOF
+
+sudo systemctl enable haproxy --now
+sudo systemctl start haproxy
+
+# Change crc base domain
+
+MACHINE_IP=$(ip addr show | awk '/inet / && $2 !~ /^127\.0\.0\.1/ {gsub(/\/.*/, "", $2); print $2; exit}')
+
+BASE_DOMAIN="${MACHINE_IP}.nip.io"
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout nip.key -out nip.crt -subj "/CN=${BASE_DOMAIN}" -addext "subjectAltName=DNS:apps.${BASE_DOMAIN},DNS:*.apps.${BASE_DOMAIN},DNS:api.${BASE_DOMAIN}"
+oc create secret tls nip-secret --cert=nip.crt --key=nip.key -n openshift-config
+
+oc patch -p "{\"spec\": {\"host\": \"default-route-openshift-image-registry.$BASE_DOMAIN\"}}" route default-route -n openshift-image-registry --type=merge
+
+cat <<EOF > ingress-patch.yaml
+spec:
+  appsDomain: apps.${BASE_DOMAIN}
+  componentRoutes:
+  - hostname: console-openshift-console.apps.${BASE_DOMAIN}
+    name: console
+    namespace: openshift-console
+    servingCertKeyPairSecret:
+      name: nip-secret
+  - hostname: oauth-openshift.apps.${BASE_DOMAIN}
+    name: oauth-openshift
+    namespace: openshift-authentication
+    servingCertKeyPairSecret:
+      name: nip-secret
+EOF
+oc patch ingresses.config.openshift.io cluster --type=merge --patch-file=ingress-patch.yaml
+
+oc patch apiserver cluster --type=merge -p "{\"spec\":{\"servingCerts\": {\"namedCertificates\":[{\"names\":[\"api.${BASE_DOMAIN}\"],\"servingCertificate\": {\"name\": \"nip-secret\"}}]}}}"
+
+wait_for_clusteroperators
+
+USERNAME=$(crc console -ojson | jq -r '.clusterConfig.adminCredentials.username')
+PASSWORD=$(crc console -ojson | jq -r '.clusterConfig.adminCredentials.password')
+
+echo ""
+
+echo "Console available here: https://console-openshift-console.apps.${BASE_DOMAIN}"
+echo "Console Login Credentials: "
+echo "USERNAME: ${USERNAME} PASSWORD: ${PASSWORD}"
+
+echo ""
+
+echo "CLI Login Command: oc login -u ${USERNAME} -p ${PASSWORD} https://api.${BASE_DOMAIN}:6443"
